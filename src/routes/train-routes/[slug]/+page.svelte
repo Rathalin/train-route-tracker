38254081f48f002e79f094cfcb70cea063617d74
Rathalin <danielflo1997@hotmail.com>
2024-05-20 11:55:50 +0200

    Add wrn-mrz data

---
+++ src/routes/train-routes/[slug]/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { _ } from 'svelte-i18n'
+</script>
+
+<svelte:head>
+	<title>{$_('page.example.title')}</title>
+</svelte:head>
+
+<div class="mx-4 mt-4 flow-root">
+	<h1 class="h1 mt-4 mb-10">
+		<span>{$_('page.example.h1.text')}</span>
+	</h1>
+
+	<article></article>
+</div>
---
+++ src/routes/train-routes/[slug]/+page.ts
@@ -0,0 +1,173 @@
+import { error } from '@sveltejs/kit'
+
+export function load({ params }) {
+	switch (params.slug) {
+		case 'wrn-mrz': {
+			return {
+				title: 'Wiener Neustadt - Mürzzuschlag',
+				sections: [
+					{
+						kilometer: { integer: 48, decimal: 1 },
+						data: { type: 'station', name: 'Wiener Neustadt' },
+					},
+					{
+						kilometer: { integer: 49, decimal: 0 },
+						data: { type: 'speed-increase', speed: 100 },
+					},
+					{
+						kilometer: { integer: 49, decimal: 8 },
+						data: { type: 'speed-increase', speed: 160 },
+					},
+					{
+						kilometer: { integer: 56, decimal: 6 },
+						data: { type: 'station', name: 'St. Egyden' },
+					},
+					{
+						kilometer: { integer: 62, decimal: 6 },
+						data: { type: 'station', name: 'Neunkirchen' },
+					},
+					{
+						kilometer: { integer: 67, decimal: 1 },
+						data: { type: 'station', name: 'Ternitz' },
+					},
+					{
+						kilometer: { integer: 69, decimal: 0 },
+						data: { type: 'speed-restriction', speed: 150 },
+					},
+					{
+						kilometer: { integer: 69, decimal: 9 },
+						data: { type: 'station', name: 'Pottschach' },
+					},
+					{
+						kilometer: { integer: 74, decimal: 8 },
+						data: { type: 'station', name: 'Gloggnitz' },
+					},
+					{
+						kilometer: { integer: 76, decimal: 0 },
+						data: { type: 'speed-restriction', speed: 80 },
+					},
+					{
+						kilometer: { integer: 76, decimal: 8 },
+						data: { type: 'speed-restriction', speed: 70 },
+					},
+					{
+						kilometer: { integer: 77, decimal: 6 },
+						data: { type: 'station', name: 'Schlöglmühl' },
+					},
+					{
+						kilometer: { integer: 79, decimal: 2 },
+						data: { type: 'speed-increase', speed: 80 },
+					},
+					{
+						kilometer: { integer: 81, decimal: 8 },
+						data: { type: 'station', name: 'Payerbach-Reichenau' },
+					},
+					{
+						kilometer: { integer: 82, decimal: 0 },
+						data: { type: 'speed-restriction', speed: 70 },
+					},
+					{
+						kilometer: { integer: 84, decimal: 8 },
+						data: { type: 'station', name: 'Küb' },
+					},
+					{
+						kilometer: { integer: 84, decimal: 9 },
+						data: { type: 'speed-restriction', speed: 60 },
+					},
+					{
+						kilometer: { integer: 88, decimal: 1 },
+						data: { type: 'station', name: 'Eichberg' },
+					},
+					{
+						kilometer: { integer: 88, decimal: 8 },
+						data: { type: 'speed-increase', speed: 70 },
+					},
+					{
+						kilometer: { integer: 91, decimal: 8 },
+						data: { type: 'speed-restriction', speed: 60 },
+					},
+					{
+						kilometer: { integer: 92, decimal: 2 },
+						data: { type: 'station', name: 'Klamm-Schottwien' },
+					},
+					{
+						kilometer: { integer: 97, decimal: 6 },
+						data: { type: 'station', name: 'Breitenstein' },
+					},
+					{
+						kilometer: { integer: 100, decimal: 2 },
+						data: { type: 'speed-restriction', speed: 50 },
+					},
+					{
+						kilometer: { integer: 102, decimal: 0 },
+						data: { type: 'station', name: 'Wolfsbergkogel' },
+					},
+					{
+						kilometer: { integer: 103, decimal: 2 },
+						data: { type: 'station', name: 'Semmering' },
+					},
+					{
+						kilometer: { integer: 103, decimal: 4 },
+						data: { type: 'speed-restriction', speed: 40 },
+					},
+					{
+						kilometer: { integer: 103, decimal: 5 },
+						data: { type: 'speed-increase', speed: 65 },
+					},
+					{
+						kilometer: { integer: 105, decimal: 2 },
+						data: { type: 'speed-increase', speed: 80 },
+					},
+					{
+						kilometer: { integer: 107, decimal: 7 },
+						data: { type: 'station', name: 'Steinhaus' },
+					},
+					{
+						kilometer: { integer: 110, decimal: 5 },
+						data: { type: 'station', name: 'Spital am Semmering' },
+					},
+					{
+						kilometer: { integer: 115, decimal: 0 },
+						data: { type: 'speed-restriction', speed: 40 },
+					},
+					{
+						kilometer: { integer: 116, decimal: 7 },
+						data: { type: 'station', name: 'Mürzzuschlag' },
+					},
+				],
+			} satisfies TrainRoute
+		}
+		default:
+			return error(404, 'Not Found')
+	}
+}
+
+type SectionData =
+	| {
+			type: 'station'
+			name: string
+	  }
+	| {
+			type: 'speed-restriction'
+			speed: number
+	  }
+	| {
+			type: 'speed-increase'
+			speed: number
+	  }
+	| {
+			type: 'neutral-section'
+	  }
+
+type Section = {
+	kilometer: {
+		integer: number
+		decimal: number
+	}
+	data: SectionData
+}
+
+type TrainRoute = {
+	title: string
+	sections: Section[]
+}
