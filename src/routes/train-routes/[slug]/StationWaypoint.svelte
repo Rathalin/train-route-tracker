c6d6f061f6e005533ddf5be3ba0d8b08d044d2bb
Rathalin <danielflo1997@hotmail.com>
2024-05-21 18:35:17 +0200

    Change station icon to Octagon

--- src/routes/train-routes/[slug]/StationWaypoint.svelte
+++ src/routes/train-routes/[slug]/StationWaypoint.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
 	import TableColumn from './TableColumn.svelte'
-	import HomeIcon from 'svelte-material-icons/Home.svelte'
+	import OctagonIcon from 'svelte-material-icons/Octagon.svelte'
 
 	export let stationName: string
 	export let notes: string
 </script>
 
-<TableColumn><span class="text-tertiary-400"><HomeIcon /></span></TableColumn>
+<TableColumn><span class="text-tertiary-400"><OctagonIcon /></span></TableColumn>
 <TableColumn><span>{stationName}</span></TableColumn>
 <TableColumn><span>{notes}</span></TableColumn>
